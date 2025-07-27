9e7e3fd05bb6d7e72eb9189d70e7e4c2debbd7e0
Iqbaalaji <ajieeevest@gmail.com>
2025-07-27 15:59:35 +0700

    add id

--- src/app/dashboard/certificates/page.tsx
+++ src/app/dashboard/certificates/page.tsx
@@ -172,7 +172,7 @@ export default function CertificatesPage() {
           animate={{ opacity: 1 }}
           className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
         >
-          {filteredCertificates.map((certificate) => (
+          {filteredCertificates.map((certificate, index) => (
             <motion.div
               key={certificate.id}
               initial={{ opacity: 0, y: 20 }}
@@ -198,6 +198,7 @@ export default function CertificatesPage() {
                     </div>
                     <Link href={`/dashboard/certificates/${certificate.id}`}>
                       <Button
+                        id={"key-" + index.toString()}
                         variant="outline"
                         size="sm"
                         className="text-sm w-[80px] cursor-pointer"
